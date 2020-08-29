86c16dfc29a2715e581bc624195c2b428e3e19f4
coderealistics <coderealistics@yandex.com>
2020-08-29 10:51:21 +0500

    Add Environment Vars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const port = 3000;
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -19,6 +23,15 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useUnifiedTopology: true,
 });
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
 app.get("/", function (req, res) {
   res.render("Home");
 });
@@ -29,6 +42,42 @@ app.get("/login", function (req, res) {
 
 app.get("/register", function (req, res) {
   res.render("Register");
+});
+
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+      console.log("secrets successfully reg!");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username,
+    function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+            console.log("secrets successfully log!");
+          }
+        }
+      }
+    },
+  });
 });
 
 app.listen(port, function () {
